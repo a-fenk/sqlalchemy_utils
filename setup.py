938246a796598e7a0407539b83c262c3a2657efd
afenk <artyom.antnv@gmail.com>
2022-08-20 22:12:38 +0300

    int_enum

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+with open('requirements.txt', 'r') as f:
+    requirements = f.read().split('\n')
+
+setuptools.setup(
+    name='sqlalchemy_utils',
+    version='0.0.1',
+    author='Tom Antonov',
+    author_email='artyom.antnv@gmail.com',
+    description='...',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://gitlab.com/skipp1/S954_Skipp3_0/rabbitmq_client',
+    packages=setuptools.find_packages(),
+    install_requires=[
+
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.10',
+)
---
+++ sqlalchemy_utils/int_enum.py
@@ -0,0 +1,18 @@
+from sqlalchemy import TypeDecorator, Integer
+
+
+class IntEnum(TypeDecorator):
+    impl = Integer
+
+    def __init__(self, enumtype, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._enumtype = enumtype
+
+    def process_bind_param(self, value, dialect):
+        if value:
+            return value.value if not isinstance(value, int) else value
+        else:
+            return None
+
+    def process_result_value(self, value, dialect):
+        return self._enumtype(value) if value is not None else None
